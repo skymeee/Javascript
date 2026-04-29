e065c9cf370a33734c6f794cc6534687a88f16c5
skymeee <tanishq041200@gmail.com>
2026-04-29 14:22:30 +0000

    let and constant

---
+++ .devcontainer/01_basic/test.js
@@ -0,0 +1,15 @@
+ const accountid = 1423 
+let accountemail ="shivani2.vom"
+var accountpaassword = "76543"
+accountcity = "mumbai"
+let accountstate;
+//  account id not chnage because constant cannot chnaged after the assigned
+//var and let can be change ' always use let instead of  var
+accountemail="shivanigmail.com"
+accountpaassword = "12345"
+accountcity = "thane"
+console.log(accountid);
+console.table([accountid, accountemail, accountpaassword,accountcity, accountstate])
+/*
+prefer not use var becaus of issue in block scope and functional scope
+*/
